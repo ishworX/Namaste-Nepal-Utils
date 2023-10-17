5ff37d1d4aed626cce44f8f3bef1518d4462d121
ishworX <git.ishwor@gmail.com>
2023-10-17 15:09:54 -0600

    Command update!

--- commands/clear.js
+++ commands/clear.js
@@ -22,51 +22,13 @@ module.exports = {
             await queue.stop(interaction.guild.id);
 
             const embed = new EmbedBuilder()
-                .setColor('#const db = require("../mongoDB");
-const { EmbedBuilder } = require('discord.js');
-
-            module.exports = {
-                name: "clear",
-                description: "Clears the music queue.",
-                permissions: "0x0000000000000800",
-                options: [],
-                voiceChannel: true,
-                run: async (client, interaction) => {
-                    const queue = client.player.getQueue(interaction.guild.id);
-
-                    try {
-                        if (!queue || !queue.playing) {
-                            return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
-                        }
-
-                        if (!queue.songs[0]) {
-                            return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true });
-                        }
-
-                        await queue.stop(interaction.guild.id);
-
-                        const embed = new EmbedBuilder()
-                            .setColor('#3498db')
-                            .setAuthor({
-                                name: 'Cleared List',
-                                iconURL: 'https://media.discordapp.net/attachments/1115193942724595804/1163919889107525662/queue..png?ex=654153de&is=652edede&hm=bc41a5b7c52bed5d7df71f5892288028eaa623296c2bce35fd1907f3694fd14c&=&width=802&height=840',
-                                url: 'https://discord.gg/nhj6yAqUdG'
-                            })
-                            .setDescription('**Queue cleared! Be Ready for a new musical journey.**')
-
-
-                        interaction.reply({ embeds: [embed] });
-                    } catch (e) {
-                        console.error(e);
-                    }
-                },
-            }; ')
+                .setColor('#7645fe')
                 .setAuthor({
                     name: 'Cleared List',
-                    iconURL: 'https://media.discordapp.net/attachments/1115193942724595804/1163919889107525662/queue..png?ex=654153de&is=652edede&hm=bc41a5b7c52bed5d7df71f5892288028eaa623296c2bce35fd1907f3694fd14c&=&width=802&height=840',
+                    iconURL: 'https://media.discordapp.net/attachments/1115193942724595804/1163898916723494993/pause-button-6075357-5012992.webp?ex=65414055&is=652ecb55&hm=6a2e3e590d571cd05b357469d56a74a71add139e6817f01f5026c6e0f04e948e&=&width=900&height=900',
                     url: 'https://discord.gg/nhj6yAqUdG'
                 })
-                .setDescription('**Queue has been cleared successfully.! **')
+                .setDescription('**Queue has been cleared successfully.**')
 
 
             interaction.reply({ embeds: [embed] });
